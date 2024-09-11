c395a5f71552652c423ed749e1057b7065e526a8
bnakamura <bnakamura@binn.biz>
2024-09-11 15:52:03 +0900

    commit

--- web/apps/app.py
+++ web/apps/app.py
@@ -1,10 +1,10 @@
 from distutils.log import Log
 from flask import Flask, jsonify
-from flask_debugtoolbar import DebugToolbarExtension
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
-from flask_mail import Mail
+#from flask_debugtoolbar import DebugToolbarExtension
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
+#from flask_wtf.csrf import CSRFProtect
+#from flask_mail import Mail
 
 import importlib
 import logging
